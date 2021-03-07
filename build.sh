56469618a9b7fe9c709d946fc3744c2d2b6853f8
Ridhwan Dewoprabowo <ridhwan.dewoprabowo@gmail.com>
2021-03-07 16:08:18 +0100

    Update build.sh
    Tested on CentOS 8. Installation successful. Still problem on connection between htd and dpdb,

--- build.sh
+++ build.sh
@@ -1,49 +1,10 @@
 #!/bin/bash
-# Installation v0.3 for CentOS
+# Installation v0.4
 
 echo "===== DPDB and Dependencies Installer ====="
-sudo yum update
-
-# Install Postgres
-echo "===== Install and setup PostgreSQL ====="
-sudo yum -y install postgresql-server postgresql-contrib
-sudo postgresql-setup initdb
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
-sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'XXX';"			# ?
-sudo -u postgres psql -c "CREATE USER logicsem WITH PASSWORD 'XXX';"
-sudo -u postgres psql -c "CREATE DATABASE logicsem;"
-
-# Assume git, gcc, cmake, and doxygen?
-
-# Install gcc, g++, openssl
-sudo yum -y install gcc
-sudo yum -y install gcc-c++
-sudo yum -y install openssl-devel
-
-# Install cmake
-cd ../
-wget https://github.com/Kitware/CMake/releases/download/v3.19.6/cmake-3.19.6.tar.gz --no-check-certificate
-tar -zxvf cmake-3.19.6.tar.gz
-cd cmake-3.19.6
-./bootstrap && make && sudo make install
-cmake --version
-cd ../dp_on_dbs
-
-# Install doxygen
-sudo yum -y install doxygen
-
-# Install htd
-echo "===== Install htd ====="
-sudo yum -y install git
-git clone -b normalize_cli --single-branch https://github.com/TU-Wien-DBAI/htd.git ../htd
-cd ../htd
-cmake ./ && make
-make install
-cd ../dp_on_dbs
 
 # Install Anaconda
-echo "===== Install and setup Anaconda ====="
+echo "===== Install and Setup Anaconda ====="
 cd ../
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --no-check-certificate
 chmod +x Miniconda3-latest-Linux-x86_64.sh
@@ -54,3 +15,36 @@ echo "===== Create Anaconda Environment for DPDB ====="
 cd dp_on_dbs
 conda env create -f environment.yml
 conda activate nesthdb
+
+# Install Postgres
+echo "===== Install and setup PostgreSQL ====="
+conda install -y -c anaconda postgresql
+initdb -D mylocal_db
+pg_ctl -D mylocal_db -l logfile start
+createuser -s postgres
+psql postgres -c "ALTER USER postgres PASSWORD 'XXX';"			# ?
+psql postgres -c "CREATE USER logicsem WITH PASSWORD 'XXX';"
+psql postgres -c "CREATE DATABASE logicsem;"
+
+# Assume git, gcc, cmake, and doxygen?
+
+# Install gcc, g++, make, cmake, doxygen
+echo "===== Install GCC ====="
+conda install -y -c anaconda gcc_linux-64
+echo "===== Install G++ ====="
+conda install -y -c anaconda gxx_linux-64
+echo "===== Install Make ====="
+conda install -y -c anaconda make
+echo "===== Install CMake ====="
+conda install -y -c anaconda cmake
+echo "===== Install Doxygen ====="
+conda install -y -c conda-forge doxygen
+
+# Install htd
+echo "===== Install htd ====="
+conda install -y -c anaconda git
+git clone -b normalize_cli --single-branch https://github.com/TU-Wien-DBAI/htd.git ../htd
+cd ../htd
+cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX .
+make && make install
+cd ../dp_on_dbs
