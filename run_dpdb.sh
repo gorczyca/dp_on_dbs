6e388c64ec1fac8e31d160e0901b574e229d1242
Johannes Fichte <johannes.fichte@tu-dresden.de>
2021-03-29 00:08:42 +0200

    Minors

--- run_dpdb.sh
+++ run_dpdb.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 basepath=$(dirname $(realpath $0))
 
+cd $(dirname $0)
+
 $basepath/initdb.sh > /dev/stderr
-python $basepath/dpdb.py -f $1 $2 --input-format tgf
+echo "===================="
+echo "SETUP DONE"
+python $basepath/dpdb.py -f $1 $2 --input-format apx
