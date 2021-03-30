a71bc003a83acd7f6ae2b77e739e654b8717d02e
Piotr Gorczyca <gorczycapj@gmail.com>
2021-03-30 16:49:32 +0200

    Modified build.sh; added check if env already created; improved readability

--- build.sh
+++ build.sh
@@ -1,28 +1,52 @@
 #!/usr/bin/env bash
 # Installation v0.6
 
-echo "===== DPDB and Dependencies Installer ====="
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+CYAN='\033[1;36m'
+NC='\033[0m' # No Color
 
-# Install Anaconda
-echo "===== Install and Setup Anaconda ====="
+echo
+echo -e $GREEN"DPDB: Installation started."$NC
+echo
+
+echo
+echo -e $GREEN"DPDB: Installing conda."$NC
+echo
 cd ../
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --no-check-certificate
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
 . ~/miniconda3/etc/profile.d/conda.sh
 
-echo "===== Create Anaconda Environment for DPDB ====="
+echo
+echo -e $GREEN"DPDB: Creating conda environment."$NC
+echo
 cd dp_on_dbs
-conda env create -f environment.yml
-# conda activate dpdb_env
+if conda info --envs | grep dpdb_env; then
+  echo
+  echo -e $CYAN"DPDB: dpdb_env environment already created."$NC
+  echo -e $CYAN"DPDB: skipping creating conda environment."$NC
+  echo
+else
+  echo
+  echo -e $GREEN"DPDB: creating conda environment: dpdb_env."$NC
+  echo
+  conda env create -f environment.yml
+  echo
+  echo -e $GREEN"DPDB: conda environment created."$NC
+  echo
+fi
 
-echo "===== Make binaries and scripts executable ====="
+echo
+echo -e $GREEN"DPDB: Making files executable."$NC
+echo
 chmod +x ./solver.sh
 chmod +x ./run_dpdb.sh
 chmod +x ./initdb.sh
 # chmod +x ./d4_bash.sh
 chmod +x ./binaries/*
 
-# echo "To use dpdb:"
-# echo "Start conda environment: conda activate dpdb_env"
-echo "===== Done ====="
+echo
+echo -e $GREEN"DPDB: Installation finished."$NC
+echo
